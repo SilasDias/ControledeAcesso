2943a10a8f56abaa9256225042359d80944aa998
silas.dias <silasdjd@gmail.com>
2021-08-03 12:32:17 -0300

    Criando Crud Jornada

--- src/main/java/com/dio/controledeponto/controller/JornadaTrabalhoController.java
+++ src/main/java/com/dio/controledeponto/controller/JornadaTrabalhoController.java
@@ -1,16 +1,36 @@
 package com.dio.controledeponto.controller;
 
 import com.dio.controledeponto.model.JornadaTrabalho;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dio.controledeponto.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/jornada")
 public class JornadaTrabalhoController {
 
-    @PostMapping
-    public JornadaTrabalho createJornada(){
-        return null;
+    private final JornadaService jornadaService;
+
+    @Autowired
+    public JornadaTrabalhoController(JornadaService jornadaService) {
+        this.jornadaService = jornadaService;
     }
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+    public List<JornadaTrabalho> listAll(){
+        return jornadaService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public JornadaTrabalho findById(@PathVariable Long id) {
+        return jornadaService.findById(id);
+    }
+
+
 }
--- src/main/java/com/dio/controledeponto/service/JornadaService.java
+++ src/main/java/com/dio/controledeponto/service/JornadaService.java
@@ -4,18 +4,32 @@ import com.dio.controledeponto.model.JornadaTrabalho;
 import com.dio.controledeponto.repository.JornadaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JornadaService {
 
-    JornadaRepository jornadaRepository;
+    private JornadaRepository jornadaRepository;
 
     @Autowired // Initialize
     public JornadaService(JornadaRepository jornadaRepository){
         this.jornadaRepository = jornadaRepository;
     }
 
-    public JornadaTrabalho save(JornadaTrabalho jornadaTrabalho) {
+    public JornadaTrabalho saveJornada(JornadaTrabalho jornadaTrabalho) {
         return jornadaRepository.save(jornadaTrabalho);
+    }
+
+    public List<JornadaTrabalho> listAll() {
+        List<JornadaTrabalho> allJornadaTrabalhos = jornadaRepository.findAll();
+        return allJornadaTrabalhos;
+    }
+
+    public JornadaTrabalho findById(@PathVariable Long id) {
+        Optional<JornadaTrabalho> optionalJornada = jornadaRepository.findById(id);
+        return optionalJornada.get();
     }
 }
